1e9a8fc5a31eecaeaf7430c03f1141aed124de09
art <kartartur@mail.ru>
2023-12-29 20:29:45 +0200

    Initial Commit

---
+++ MovieDb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDb
+//
+//  Created by MacBook Pro 2015 on 29.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
